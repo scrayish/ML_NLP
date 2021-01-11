5242582f6f027cdd7fbb52d8dfb0372c535a5887
Matīss Locāns <matiss.locans@gmail.com>
2021-01-11 22:14:44 +0200

    Resurrection from ashes

--- models/Transformer.py
+++ models/Transformer.py
@@ -19,111 +19,105 @@ import torch.nn as nn
 import numpy as np
 from BuildBlocks.EncodingLayer import EncodingLayer
 from BuildBlocks.DecodingLayer import DecodingLayer
-
+from BuildBlocks.SinCosEmbedding import PositionalEncoding
 
 # BuildBlocks model class:
 class Model(nn.Module):
-    def __init__(self, args, embeddings):
+    def __init__(self, args, end_token, sine_position=False):
         super(Model, self).__init__()
-        # Define embeddings (word and positional):
-        self.embedding_dims = 300
-        embeddings.append(torch.rand((self.embedding_dims,)))
-        embeddings.insert(0, torch.zeros((self.embedding_dims,)))
-        embedding_table = torch.stack(embeddings)
-        self.embedding_length = len(embedding_table)
-        self.embedding_word = torch.nn.Embedding.from_pretrained(
-            embeddings=embedding_table,
-            freeze=False,
-            padding_idx=0,
-        )
-        # Currently hard-coding, no need to get big values since not many long sequences and not too much to context:
-        self.embedding_positional = torch.nn.Embedding(
-            num_embeddings=100,
-            embedding_dim=300,
-            padding_idx=0,
-        )
 
-        # Define parameters for model structure:
+        # Parameters for building Transformer:
         self.s_a_unit_count = args.s_a_unit_count
         self.dimensions = args.dimensions
-        self.ff_inner_dim = 1200   # In original paper inner dims = 4 * embedding dims
+        self.ff_inner_dim = self.dimensions * 4
         self.layer_count = args.layer_count
 
-        # Define Encoder block, using ModuleList:
-        self.encoder = torch.nn.ModuleList(
-            [
-                EncodingLayer(
-                    s_a_unit_count=self.s_a_unit_count,
-                    dimensions=self.dimensions,
-                    embedding_dims=self.embedding_dims,
-                    ff_inner_dim=self.ff_inner_dim,
-                    need_mask=True,
-                ) for i in range(self.layer_count)
-            ]
+        # Embedding definitions:
+        # Word embedding:
+        self.embedding_word = torch.nn.Embedding(
+            num_embeddings=end_token + 1,
+            embedding_dim=self.dimensions,
+            padding_idx=0,
         )
 
-        self.decoder = torch.nn.ModuleList(
-            [
-                DecodingLayer(
-                    s_a_unit_count=self.s_a_unit_count,
-                    dimensions=self.dimensions,
-                    embedding_dims=self.embedding_dims,
-                    ff_inner_dim=self.ff_inner_dim,
-                ) for i in range(self.layer_count)
-            ]
-        )
+        # Positional embedding - can be either sin/cos or table (same as word):
+        # Positional embedding is based on how long is longest sequence, need to know this beforehand;
+        # Could leave headroom as is done here, as longest sequence is 93 words:
+        if sine_position:
+            self.embedding_positional = PositionalEncoding(
+                num_embeddings=100,
+                embedding_dim=self.dimensions,
+            )
+        else:
+            self.embedding_positional = torch.nn.Embedding(
+                num_embeddings=100,
+                embedding_dim=self.dimensions,
+                padding_idx=0,
+            )
 
-        # Define final fully-connected layer (returns OHE sized vectors) and Softmax classifier:
-        self.final_fc_layer = torch.nn.Linear(in_features=self.embedding_dims, out_features=self.embedding_length)
+            # Disable gradient, in testing enabled gradient didn't really work:
+            self.embedding_positional.requires_grad_(False)
+
+        # Define Encoder:
+        self.encoder = torch.nn.ModuleList([
+            EncodingLayer(
+                s_a_unit_count=self.s_a_unit_count,
+                dimensions=int(self.dimensions / self.s_a_unit_count),
+                embedding_dims=self.dimensions,
+                ff_inner_dim=self.ff_inner_dim,
+                need_mask=False,
+            ) for i in range(self.layer_count)
+        ])
+
+        # Define Decoder:
+        self.decoder = torch.nn.ModuleList([
+            DecodingLayer(
+                s_a_unit_count=self.s_a_unit_count,
+                dimensions=int(self.dimensions / self.s_a_unit_count),
+                embedding_dims=self.dimensions,
+                ff_inner_dim=self.ff_inner_dim,
+            ) for i in range(self.layer_count)
+        ])
+
+        self.final_linear = torch.nn.Linear(self.dimensions, end_token + 1)
         self.softmax = torch.nn.Softmax(dim=2)
 
-    def forward(self, x, return_matrix):
-        # Send through embeddings to prepare data:
-        # Get indices for values in sentences to apply positional encoding:
-        sentence_nums_x = []
-        sentence_nums_y = []
-        for sequence in x:
+    def forward(self, x, y, return_matrix):
 
-            # Build sequence using the length of sequence:
-            sequence_positional_indices_x = []
-            sequence_positional_indices_y = []
-            for idx, token in enumerate(sequence, 1):
-                if token == 0:
-                    sequence_positional_indices_x.append(0)
-                    sequence_positional_indices_y.append(0)
-                else:
-                    sequence_positional_indices_x.append(idx)
-                    sequence_positional_indices_y.append(idx + 1)
-
-            sequence_positional_indices_x = np.asarray(sequence_positional_indices_x, dtype=np.int32)
-            sequence_positional_indices_y = np.asarray(sequence_positional_indices_y, dtype=np.int32)
-            sentence_nums_x.append(sequence_positional_indices_x)
-            sentence_nums_y.append(sequence_positional_indices_y)
-
-        # Turn all of them into tensors for passing through embedding for position:
-        sentence_nums_x = torch.LongTensor(sentence_nums_x)
-        sentence_nums_y = torch.LongTensor(sentence_nums_y)
-
-        # Embed x values:
+        # Transform input to embeddings:
+        index_matrix_x = np.asarray([
+            np.arange(1, x.size(1) + 1) for i in range(x.size(0))
+        ])
+        index_matrix_y = index_matrix_x + 1
+        remove_padded_grid = (x != 0).int().numpy()
+        index_matrix_x = torch.LongTensor(
+            index_matrix_x * remove_padded_grid
+        ).to(x.device)
         x_embedded = self.embedding_word.forward(x)
+        x_encoded = x_embedded * self.embedding_positional.forward(index_matrix_x)
 
-        # Positional encoding for inputs/outputs:
-        x_positional = self.embedding_positional(sentence_nums_x)
-        y_positional = self.embedding_positional(sentence_nums_y)
-        x_encoded = x_embedded + x_positional
+        # Pass values through embeddings, pass positions through embeddings and add together:
+        # Configure y to all ones for freedom of movement for rollout transformer:
+        if y is None:
+            y_encoded = torch.ones_like(x_encoded)
+        else:
+            index_matrix_y = torch.LongTensor(
+                index_matrix_y * remove_padded_grid
+            ).to(x.device)
+            y_embedded = self.embedding_word.forward(y)
+            y_encoded = y_embedded * self.embedding_positional.forward(index_matrix_y)
 
-        # Go through encoder stack and get the result:
-        for encoding_layer in self.encoder:
-            x_encoded, matrix = encoding_layer.forward(x_encoded, return_matrix)
+        # Pass x values through encoder part:
+        matrix = None
+        for layer in self.encoder:
+            x_encoded, matrix = layer.forward(x_encoded, return_matrix)
 
-        # Now go through decoder stack and get result:
-        # Apply positional encoding to encoder output for first "output" input in decoder:
-        # Put y_encoded in decoder_result, so can use same variable for recursive passing:
-        decoder_result = x_encoded + y_positional
-        for decoding_layer in self.decoder:
-            decoder_result = decoding_layer.forward(x_encoded, decoder_result)
+        # Pass values into decoder part:
+        y_out = y_encoded
+        for layer in self.decoder:
+            y_out, matrix = layer.forward(x_encoded, y_out)
 
-        # Go through final layers (small eps to avoid NaN):
-        out = self.final_fc_layer.forward(decoder_result)
-        out = self.softmax.forward(out + 1e-16)
+        # Final linear layer and softmax over it:
+        out = self.final_linear(y_out)
+        out = self.softmax(out)
         return out, matrix
