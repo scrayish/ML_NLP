9ddfc606af8482032007d5076b4943e3a6eb5d13
Matīss Locāns <matiss.locans@gmail.com>
2021-01-11 22:13:56 +0200

    Small adjustments

--- BuildBlocks/EncodingLayer.py
+++ BuildBlocks/EncodingLayer.py
@@ -11,7 +11,6 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 from BuildBlocks.MultiHeadAttentionUnit import MultiHeadAttentionUnit
-from torch.nn import MultiheadAttention
 
 
 # Encoding layer class:
@@ -19,16 +18,11 @@ class EncodingLayer(nn.Module):
     def __init__(self, s_a_unit_count, dimensions, embedding_dims, ff_inner_dim, need_mask: bool):
         super(EncodingLayer, self).__init__()
         # All size parameters:
-        self.s_a_units = s_a_unit_count
+        self.s_a_unit_count = s_a_unit_count
         self.dimensions = dimensions
         self.embedding_dims = embedding_dims
-        self.ff_inner_dimension = ff_inner_dim
+        self.ff_inner_dim = ff_inner_dim
         self.masked = need_mask
-
-        # Linear layers for getting Q, K, V matrices:
-        self.q_linear = torch.nn.Linear(in_features=self.embedding_dims, out_features=self.embedding_dims)
-        self.k_linear = torch.nn.Linear(in_features=self.embedding_dims, out_features=self.embedding_dims)
-        self.v_linear = torch.nn.Linear(in_features=self.embedding_dims, out_features=self.embedding_dims)
 
         # Define layer normalization (LayerNorm) https://arxiv.org/pdf/1607.06450.pdf:
         self.layer_norm_1 = torch.nn.LayerNorm(normalized_shape=self.embedding_dims)
@@ -36,7 +30,7 @@ class EncodingLayer(nn.Module):
 
         # Multi-head attention unit:
         self.multi_head_attention_unit = MultiHeadAttentionUnit(
-            unit_count=self.s_a_units,
+            s_a_unit_count=self.s_a_unit_count,
             dimensions=self.dimensions,
             embedding_dims=self.embedding_dims,
             need_mask=self.masked,
@@ -44,9 +38,9 @@ class EncodingLayer(nn.Module):
 
         # Feed forward neural network layers:
         self.feed_forward = torch.nn.Sequential(
-            torch.nn.Linear(in_features=self.embedding_dims, out_features=self.ff_inner_dimension),
+            torch.nn.Linear(in_features=self.embedding_dims, out_features=self.ff_inner_dim),
             torch.nn.ReLU(),
-            torch.nn.Linear(in_features=self.ff_inner_dimension, out_features=self.embedding_dims),
+            torch.nn.Linear(in_features=self.ff_inner_dim, out_features=self.embedding_dims),
         )
 
     def forward(self, x, return_matrix):
