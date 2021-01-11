5d4f64422f62bb16a4003505561d1dafd7309301
Matīss Locāns <matiss.locans@gmail.com>
2021-01-11 22:14:33 +0200

    Removed class to separate file

--- models/GPT.py
+++ models/GPT.py
@@ -11,6 +11,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from BuildBlocks.EncodingLayer import EncodingLayer
+from BuildBlocks.SinCosEmbedding import PositionalEncoding
 
 
 # GPT Model class:
@@ -94,19 +95,3 @@ class Model(nn.Module):
         out = torch.matmul(x_encoded, self.embedding_word.weight.t())
         out = self.softmax.forward(out)
         return out, matrix
-
-
-# Sin/Cos positional encoding:
-class PositionalEncoding(torch.nn.Module):
-
-    def __init__(self, num_embeddings, embedding_dim):
-        super(PositionalEncoding, self).__init__()
-
-        self.pe = torch.zeros(num_embeddings, embedding_dim)
-        position = torch.arange(0, num_embeddings, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, embedding_dim, 2).float() * (-np.log(10000.0) / embedding_dim))
-        self.pe[:, 0::2] = torch.sin(position * div_term)
-        self.pe[:, 1::2] = torch.cos(position * div_term)
-
-    def forward(self, idxes):
-        return self.pe[idxes, :]
